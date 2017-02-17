613c9b774623b6bab4ada33d6af0bd1b91197bf0
Jonathan Richard Robert Kimmitt <jrrk2@cl.cam.ac.uk>
2017-02-17 13:12:37 +0000

    Remove thread library dependence

---
+++ bare_metal/uart.c
@@ -0,0 +1,144 @@
+// See LICENSE for license details.
+
+#include <stdio.h>
+#include <sys/ioctl.h>
+#include "uart.h"
+#include "minion_lib.h"
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <termios.h>
+
+int fd, first = 1;
+
+static struct termios oldt;
+
+void normal(void)
+{
+  tcgetattr(fd, &oldt);
+  oldt.c_lflag |= (ICANON|ECHO);
+  oldt.c_iflag |= (ICRNL);
+  tcsetattr(fd, 0, &oldt);
+  close(fd);
+  first = 1;
+}
+
+void cbreak(void)
+{
+  int rslt;
+  first = 0;
+  fd = open("/dev/tty", O_RDWR);
+  rslt = tcgetattr(fd, &oldt);
+  if (rslt) perror("tcgetattr");
+  oldt.c_lflag &= ~ (ICANON|ECHO);
+  oldt.c_iflag &= ~ (ICRNL);
+  rslt = tcsetattr(fd, 0, &oldt);
+  if (rslt) perror("tcsetattr");
+  atexit(normal);
+}
+
+void uart_sendchar(char ch)
+{
+  if (first) cbreak();
+  write(fd, &ch, 1);
+}
+
+int uart_getchar(void)
+{
+  int ch, cnt;
+  if (first) cbreak();
+  cnt = read(fd, &ch, 1);
+  return (cnt < 0) ? 4 : ch == '\r' ? '\n' : ch;
+}
+
+int uart_tstchar(void)
+{
+  int ch;
+  int rslt, cnt = 0;
+  if (first) cbreak();
+  rslt = ioctl(fd, FIONREAD, &cnt);
+  return cnt > 0;
+}
+
+volatile uint32_t *uart_base_ptr = (uint32_t *)(UART_BASE);
+
+void uart_init() {
+
+}
+
+void minion_console_putchar(unsigned char ch)
+{
+  static int addr_int = 0;
+  volatile uint32_t * const video_base = (volatile uint32_t*)(10<<20);
+  switch (ch)
+    {
+    case '\b':
+      if (addr_int & 127) addr_int--;
+      break;
+    case '\r':
+      break;
+    case '\n':
+      while ((addr_int & 127) < 127)
+	queue_write(&(video_base[addr_int++]), ' ', 1);
+      ++addr_int;
+      break;
+    default:
+      queue_write(&video_base[addr_int++], ch, 0);
+      break;
+    }
+  addr_int &= 4095;
+}
+
+void uart_send(uint8_t data) {
+  uart_sendchar(data);
+  minion_console_putchar(data);
+}
+
+void uart_send_string(const char *str) {
+  while (*str) uart_send(*str++);
+}
+
+void uart_send_buf(const char *buf, const int32_t len) {
+  int32_t i;
+  for (i=0; i<len; i++) uart_send(buf[i]);
+}
+
+// check uart IRQ for read
+uint8_t uart_check_read_irq() {
+  return uart_tstchar();
+}
+
+// IRQ triggered read
+uint8_t uart_read_irq() {
+  return uart_getchar();
+}
+
+uint8_t uart_recv() {
+  // wait until RBR has data
+  while(! uart_check_read_irq())
+    {
+      uint32_t key;
+      volatile uint32_t * const keyb_base = (volatile uint32_t*)(9<<20);
+      key = queue_read(keyb_base);
+      if ((1<<28) & ~key) /* FIFO not empty */
+	{
+	  int ch;
+	  queue_write(keyb_base+1, 0, 0);
+	  ch = (queue_read(keyb_base+1) >> 8) & 127; /* strip off the scan code (default ascii code is UK) */
+	  if (ch == '\r') ch = '\n'; /* translate CR to LF, because nobody else will */
+	  return ch;
+	}
+    }
+  return uart_read_irq();
+}
+
+// enable uart read IRQ
+void uart_enable_read_irq() {
+  *(uart_base_ptr + UART_IER) = 0x0001u;
+}
+
+// disable uart read IRQ
+void uart_disable_read_irq() {
+  *(uart_base_ptr + UART_IER) = 0x0000u;
+}
---
+++ serve.c
@@ -0,0 +1,126 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include "main.h"
+
+#define BUF_SIZE 500
+
+int main(int argc, char *argv[])
+       {
+           struct addrinfo hints;
+           struct addrinfo *result, *rp;
+           int sfd, s;
+           struct sockaddr_storage peer_addr;
+           socklen_t peer_addr_len;
+           ssize_t nread;
+           char buf[BUF_SIZE];
+
+           if (argc != 2) {
+               fprintf(stderr, "Usage: %s port\n", argv[0]);
+               exit(EXIT_FAILURE);
+           }
+
+           memset(&hints, 0, sizeof(struct addrinfo));
+           hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
+           hints.ai_socktype = SOCK_DGRAM; /* Datagram socket */
+           hints.ai_flags = AI_PASSIVE;    /* For wildcard IP address */
+           hints.ai_protocol = 0;          /* Any protocol */
+           hints.ai_canonname = NULL;
+           hints.ai_addr = NULL;
+           hints.ai_next = NULL;
+
+           s = getaddrinfo(NULL, argv[1], &hints, &result);
+           if (s != 0) {
+               fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+               exit(EXIT_FAILURE);
+           }
+
+           /* getaddrinfo() returns a list of address structures.
+              Try each address until we successfully bind(2).
+              If socket(2) (or bind(2)) fails, we (close the socket
+              and) try the next address. */
+           for (rp = result; rp != NULL; rp = rp->ai_next) {
+               sfd = socket(rp->ai_family, rp->ai_socktype,
+                       rp->ai_protocol);
+               if (sfd == -1)
+                   continue;
+
+               if (bind(sfd, rp->ai_addr, rp->ai_addrlen) == 0)
+                   break;                  /* Success */
+
+               close(sfd);
+           }
+
+           if (rp == NULL) {               /* No address succeeded */
+               fprintf(stderr, "Could not bind\n");
+               exit(EXIT_FAILURE);
+           }
+
+           freeaddrinfo(result);           /* No longer needed */
+
+           /* Read datagrams and echo them back to sender */
+
+           for (;;) {
+               peer_addr_len = sizeof(struct sockaddr_storage);
+               nread = recvfrom(sfd, buf, BUF_SIZE, 0,
+                       (struct sockaddr *) &peer_addr, &peer_addr_len);
+               if (nread == -1)
+                   continue;               /* Ignore failed request */
+
+               char host[NI_MAXHOST], service[NI_MAXSERV];
+
+               s = getnameinfo((struct sockaddr *) &peer_addr,
+                               peer_addr_len, host, NI_MAXHOST,
+                               service, NI_MAXSERV, NI_NUMERICSERV);
+	       if (s == 0) switch(*buf)
+		{
+		case 'W':
+		  {
+		    uint64_t ptr;
+		    uint32_t data;
+		    int flush;
+		    if (3==sscanf(buf+1, "%lX,%X,%X", &ptr, &data, &flush))
+		      {
+			volatile uint32_t * const base = (volatile uint32_t*)ptr;
+			queue_write(base, data, flush);
+			printf("queue_write(%p, %x, %x);\n", base, data, flush);
+			if (sendto(sfd, &data, 0, 0,
+                           (struct sockaddr *) &peer_addr,
+                           peer_addr_len) != 0)
+			  fprintf(stderr, "Error sending response\n");
+		      }
+		    else
+		      printf("Received %zd bytes from %s:%s\n", nread, host, service);
+		    break;
+		  }
+		case 'R':
+		  {
+		    uint64_t ptr;
+		    uint32_t data;
+		    int flush;
+		    if (1==sscanf(buf+1, "%lX", &ptr))
+		      {
+			volatile uint32_t * const base = (volatile uint32_t*)ptr;
+			data = queue_read(base);
+			printf("queue_read(%p) => %x;\n", base, data);
+			if (sendto(sfd, &data, sizeof(data), 0,
+                           (struct sockaddr *) &peer_addr,
+                           peer_addr_len) != sizeof(data))
+			  fprintf(stderr, "Error sending response\n");
+		      }
+		    else
+		      printf("Received %zd bytes from %s:%s\n", nread, host, service);
+		    break;
+		  }
+		default:
+		      printf("Received %zd bytes from %s:%s\n", nread, host, service);
+		    break;
+		}
+               else
+                   fprintf(stderr, "getnameinfo: %s\n", gai_strerror(s));
+           }
+       }
